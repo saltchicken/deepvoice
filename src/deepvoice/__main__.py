b548e51af27f14b993620d3a26ea4a63775da744
saltchicken <john.eicher89@gmail.com>
2025-02-26 16:50:48 -0800

    Add argparse

--- src/deepvoice/__main__.py
+++ src/deepvoice/__main__.py
@@ -3,22 +3,19 @@ from tortoise.api import MODELS_DIR
 # from tortoise.api import TextToSpeech
 from tortoise.api_fast import TextToSpeech
 from tortoise.utils.audio import load_voices
-import time
+import argparse
 
-tts = TextToSpeech(models_dir=MODELS_DIR, use_deepspeed=False, kv_cache=True, half=True)
-voice_samples, conditioning_latents = load_voices(['geralt'])
 #
 # audio = tts.tts("What are you up to today?")
 # sd.play(audio[0][0], samplerate=24000)
 #
-# time.sleep(4)
+# sd.wait()
 #
 #
 
 # audio, dbg_state = tts.tts_with_preset("This is what I want you to say", k=1, voice_samples=voice_samples, conditioning_latents=conditioning_latents, preset="fast", use_deterministic_seed=1, return_deterministic_state=True, cvvp_amount=.0)
 # sd.play(audio[0][0], samplerate=24000)
-#
-# time.sleep(4)
+# sd.wait()
 
 
 # stream = tts.tts_stream("This is my hello world example", voice_samples=voice_samples, conditioning_latents=conditioning_latents, verbose=True, stream_chunk_size=40)
@@ -32,11 +29,21 @@ voice_samples, conditioning_latents = load_voices(['geralt'])
 #     #     sd.play(audio, samplerate=24000)
 
 
-def run(text, chunk_size):
-    stream = tts.tts_stream(text, voice_samples=voice_samples, conditioning_latents=conditioning_latents, verbose=True, stream_chunk_size=chunk_size)
+def run(args):
+    tts = TextToSpeech(models_dir=MODELS_DIR, use_deepspeed=False, kv_cache=True, half=True)
+    voice_samples, conditioning_latents = load_voices([args.voice])
+    stream = tts.tts_stream(args.text, voice_samples=voice_samples, conditioning_latents=conditioning_latents, verbose=True, stream_chunk_size=args.chunk_size)
     for audio_chunk in stream:
         sd.play(audio_chunk.cpu(), samplerate=24000)
         sd.wait()
 
 def main():
-    run("This is the first test, just wait until we get to the second one", 256)
+    parser = argparse.ArgumentParser(description="TTS via Tortoise")
+    parser.add_argument("text", type=str, help="Text to synthesize")
+    parser.add_argument("--chunk_size", type=int, default=256, help="Chunk size")
+    parser.add_argument("--voice", type=str, default="geralt", help="Voice name")
+
+    args = parser.parse_args()
+
+
+    run(args)
