ab62acd20a9bb33c3fb0c4097c316eea1b86dd73
saltchicken <john.eicher89@gmail.com>
2025-02-28 09:41:32 -0800

    Add flag for realtime vs deep processing

--- src/deepvoice/server.py
+++ src/deepvoice/server.py
@@ -1,14 +1,14 @@
 import sounddevice as sd
 from tortoise.api import MODELS_DIR
 from tortoise.api import TextToSpeech
-# from tortoise.api_fast import TextToSpeech
+from tortoise.api_fast import TextToSpeech as FastTextToSpeech
 from tortoise.utils.audio import load_voices
 import argparse
 import socket
 
 
 
-def handle_client(conn, addr, tts, voice_samples, conditioning_latents, chunk_size):
+def handle_client(conn, addr, tts, voice_samples, conditioning_latents, chunk_size, realtime):
     print(f"Connected to {addr}")
     data = b""
 
@@ -23,14 +23,20 @@ def handle_client(conn, addr, tts, voice_samples, conditioning_latents, chunk_si
 
     received_text = data.decode()
     print(f"Received: {received_text}")
-    audio, dbg_state = tts.tts_with_preset(received_text, k=1, voice_samples=voice_samples, conditioning_latents=conditioning_latents, preset="fast", use_deterministic_seed=1, return_deterministic_state=True, cvvp_amount=.0)
-    sd.play(audio[0][0], samplerate=24000)
-    sd.wait()
+    chunked_text = received_text.split("\n")
 
-    # stream = tts.tts_stream(received_text, voice_samples=voice_samples, conditioning_latents=conditioning_latents, verbose=True, stream_chunk_size=chunk_size)
-    # for audio_chunk in stream:
-    #     sd.play(audio_chunk.cpu(), samplerate=24000)
-    #     sd.wait()
+    for text in chunked_text:
+
+        if realtime:
+            stream = tts.tts_stream(text, voice_samples=voice_samples, conditioning_latents=conditioning_latents, verbose=True, stream_chunk_size=chunk_size)
+            for audio_chunk in stream:
+                sd.play(audio_chunk.cpu(), samplerate=24000)
+                sd.wait()
+        else:
+            audio, dbg_state = tts.tts_with_preset(text, k=1, voice_samples=voice_samples, conditioning_latents=conditioning_latents, preset="fast", use_deterministic_seed=1, return_deterministic_state=True, cvvp_amount=.0)
+            sd.play(audio[0][0], samplerate=24000)
+            sd.wait()
+
 
 
     # conn.sendall(data)
@@ -39,21 +45,25 @@ def handle_client(conn, addr, tts, voice_samples, conditioning_latents, chunk_si
     # print(f"Connection from {addr} closed")
     #
 def start_server(args):
-    tts = TextToSpeech(models_dir=MODELS_DIR, use_deepspeed=False, kv_cache=True, half=True)
+    if args.realtime:
+        tts = FastTextToSpeech(models_dir=MODELS_DIR, use_deepspeed=False, kv_cache=True, half=True)
+    else:
+        tts = TextToSpeech(models_dir=MODELS_DIR, use_deepspeed=False, kv_cache=True, half=True)
     voice_samples, conditioning_latents = load_voices([args.voice])
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((args.host, args.port))
         s.listen()
         while True:
             conn, addr = s.accept()
-            handle_client(conn, addr, tts, voice_samples, conditioning_latents, args.chunk_size)
+            handle_client(conn, addr, tts, voice_samples, conditioning_latents, args.chunk_size, args.realtime)
 
 def main():
     parser = argparse.ArgumentParser(description="DeepVoice Server")
     parser.add_argument("--host", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int, default=9986)
-    parser.add_argument("--chunk_size", type=int, default=256)
+    parser.add_argument("--chunk_size", type=int, default=512)
     parser.add_argument("--voice", type=str, default="geralt")
+    parser.add_argument("--realtime", action="store_true", help="Run with FastTextToSpeech")
     args = parser.parse_args()
 
     start_server(args)
